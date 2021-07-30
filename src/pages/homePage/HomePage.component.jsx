7385fc829263463b8a7982bf416a6f3e26bd74bb
JonathanLeeWH <leeweihao1996@outlook.com>
2021-07-30 15:40:48 +0800

    Added `NotFoundPage` component to add not found 404 status code page support
    Amended `public/index.html`'s title and latest bootstrap css cdn link.
    
    Added custom document title to pages
    
    npm packages upgraded including `bootstrap` and `react-scripts`
    
    Added missing npm peer dependencies package

--- src/pages/homePage/HomePage.component.jsx
+++ src/pages/homePage/HomePage.component.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet-async';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -5,6 +6,9 @@ import MainSearchBar from '../../components/mainSearchBar/MainSearchBar.componen
 
 const HomePage = () => (
   <Container>
+    <Helmet>
+      <title>Software Repository | Home</title>
+    </Helmet>
     <Row className="justify-content-center text-center mt-5">
       <Col md={8} xl={6}>
         <MainSearchBar />
--- src/pages/profilePage/ProfilePage.component.jsx
+++ src/pages/profilePage/ProfilePage.component.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet-async';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -5,6 +6,9 @@ import ProfileInformation from '../../components/profileInformation/ProfileInfor
 
 const ProfilePage = () => (
   <Container>
+    <Helmet>
+      <title>Software Repository | Profile</title>
+    </Helmet>
     <Row className="justify-content-center mt-5">
       <Col md={8} xl={6}>
         <ProfileInformation />

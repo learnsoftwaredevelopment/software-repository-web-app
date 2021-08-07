2f3fd7bee1ec5b8edecf2e505aebc6192363ba63
JonathanLeeWH <leeweihao1996@outlook.com>
2021-08-07 23:22:08 +0800

    Added `RecentlyAddedSoftware` and `RecentlyUpdatedSoftware` components
    Amended `SoftwareInformationPage.styles.css` to use `text-align: justify` to justify the description
    
    Resolved a bug in `AddSoftwareForm` and `EditSoftwareForm` components where previously twitter username was not present in payload to backend API endpoint
    
    Added npm package `day.js` to handle date formatting
    
    Improved `.eslintrc.js` `linebreak-style` rule to switch between windows or unix line ending depending on platform

--- src/pages/homePage/HomePage.component.jsx
+++ src/pages/homePage/HomePage.component.jsx
@@ -3,6 +3,8 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import MainSearchBar from '../../components/mainSearchBar/MainSearchBar.component';
+import RecentlyAddedSoftware from '../../components/recentlyAddedSoftware/RecentlyAddedSoftware.component';
+import RecentlyUpdatedSoftware from '../../components/recentlyUpdatedSoftware/RecentlyUpdatedSoftware.component';
 
 const HomePage = () => (
   <Container>
@@ -12,6 +14,14 @@ const HomePage = () => (
     <Row className="justify-content-center text-center mt-5">
       <Col md={8} xl={6}>
         <MainSearchBar />
+      </Col>
+    </Row>
+    <Row className="justify-content-center mt-5">
+      <Col className="my-2" md={8} xl={6}>
+        <RecentlyAddedSoftware />
+      </Col>
+      <Col className="my-2" md={8} xl={6}>
+        <RecentlyUpdatedSoftware />
       </Col>
     </Row>
   </Container>
